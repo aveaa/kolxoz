99edf860fdbcca17a13a30fd48ded3e123bded13
privetstn <yansadmay@gmail.com>
2018-07-25 18:19:01 +0300

    reload

--- index.js
+++ index.js
@@ -83,7 +83,7 @@ client.on("message", async message => {
     })
     if (prefix === false) return;
 
-    const args = message.content.slice(config.prefix.length).trim().split(/ +/g);
+    const args = message.content.slice(prefix.length).trim().split(/ +/g);
     const command = args.shift().toLowerCase();
     
 
